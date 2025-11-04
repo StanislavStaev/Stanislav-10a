f884741991c10f8cfaf16ea02325160fcf215de9
StanislavStaev <stanislavstaev09@gmail.com>
2025-11-04 19:46:46 +0200

    Add files via upload

---
+++ Практическа задача ArrayList/Практическа задача ArrayList/ArrayList.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Практическа_задача_ArrayList
+{
+    public class ArrayList
+    {
+        private const int INITIAL_SIZE = 2;
+        private string[] items;
+        private int length;
+        private int capacity;
+        public int Length => length;
+        public int Capacity => capacity;
+        public ArrayList(int size = INITIAL_SIZE)
+        {
+            capacity = size;
+            length = 0;
+            items = new string[size];
+        }
+        public void Add(string item)
+        {
+            if (length == capacity)
+            {
+                capacity *= 2;
+                string[] temp = new string[capacity];
+                for (int i = 0; i < length; i++)
+                    temp[i] = items[i];
+                items = temp;
+            }
+            items[length] = item;
+            length++;
+        }
+        public void RemoveAt(int index)
+        {
+            if (index < 0 || index >= length)
+            {
+                Console.WriteLine("Nevaliden index");
+                return;
+            }
+            Console.WriteLine($"Remove from list element {items[index]}");
+            for (int i = index; i < length - 1; i++)
+            {
+                items[i] = items[i + 1];
+            }
+            length--;
+        }
+        public void Set(int index, string newItem)
+        {
+            if (index < 0 || index >= length)
+            {
+                Console.WriteLine("nevaliden index");
+                return;
+            }
+            items[index] = newItem;
+        }
+        public string Get(int index)
+        {
+            if (index < 0 || index >= length)
+            {
+                Console.WriteLine("nevaliden index");
+                return null;
+            }
+            return items[index];
+        }
+        public void Print()
+        {
+            Console.WriteLine("sudurjanie");
+            for (int i = 0; i < length; i++)
+            {
+                Console.WriteLine($"{i + 1}. {items[i]}");
+            }
+        }
+    }
+}
+
---
+++ Практическа задача ArrayList/Практическа задача ArrayList/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Практическа_задача_ArrayList.ArrayList;
+
+namespace Практическа_задача_ArrayList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList books = new ArrayList();
+
+            Console.WriteLine($"Nachalen br element {books.Length} kapacitet {books.Capacity}");
+            Console.Write("kolko knigi");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < /*books.Length*/ /*n*/ /* 6*/; i++)
+            {
+                Console.Write($"vuvedi kniga {i + 1} ");
+                string name = Console.ReadLine();
+                books.Add(name);
+            }
+            books.Print();
+            Console.WriteLine($"br element {books.Length} kapacitet {books.Capacity}");
+            Console.Write("vuvedi index za premahvane ");
+            int index = int.Parse(Console.ReadLine());
+            books.RemoveAt(index);
+            books.Print();
+            Console.WriteLine($"br element {books.Length} kapacitet {books.Capacity}");
+            Console.Write("index za smqna ");
+            int idx = int.Parse(Console.ReadLine());
+            Console.Write("novo zaglavie");
+            string novoz = Console.ReadLine();
+            books.Set(idx, novoz);
+            books.Print();
+            Console.Write("index za pokazvane ");
+            int Indexpokaz = int.Parse(Console.ReadLine());
+            string found = books.Get(Indexpokaz);
+            Console.WriteLine($"element na pozicia {Indexpokaz} e {found}");
+        }
+    }
+}
