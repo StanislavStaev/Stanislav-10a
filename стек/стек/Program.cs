168bd258047f55fda3c1445958937c0e7c7d14cf
StanislavStaev <stanislavstaev09@gmail.com>
2025-11-17 19:19:33 +0200

    Add files via upload

---
+++ стек/стек/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace стек
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<string> dumi = new Stack<string>();
+            dumi.Push("машинки");
+           dumi.Push("маламашка");
+           dumi.Push("носталгично кебабче");
+            Console.WriteLine("добавени елементи");
+            foreach (var а in dumi)
+            {
+                Console.WriteLine(а);
+            }
+            Console.WriteLine("последна дума " + dumi.Peek());
+            Console.WriteLine("махане на елемент " + dumi.Pop());
+            foreach (var б in dumi)
+            {
+                Console.WriteLine(б);
+            }
+            Console.WriteLine("брой думи " + dumi.Count);
+        }
+    }
+}
