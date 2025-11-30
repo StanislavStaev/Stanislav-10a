be09f7601337b2b5362b7ed05e5fcd0c2bcb8b08
StanislavStaev <stanislavstaev09@gmail.com>
2025-11-30 22:44:21 +0200

    Add files via upload

---
+++ УПРАЖНЕНИЕ с НАЗЪБЕНИ и ДВУМЕРНИ МАСИВИ/УПРАЖНЕНИЕ с НАЗЪБЕНИ и ДВУМЕРНИ МАСИВИ/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace УПРАЖНЕНИЕ_с_НАЗЪБЕНИ_и_ДВУМЕРНИ_МАСИВИ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("br igra4i  ");
+            int ibr = int.Parse(Console.ReadLine());
+            Console.Write("br igri  ");
+            int gbr = int.Parse(Console.ReadLine());
+            string[] ime = new string[ibr];
+            int[][] t = new int[ibr][];
+            for (int i = 0; i < ibr; i++)
+            {
+                Console.WriteLine("igra4 "+ (i + 1) );
+                Console.Write("ime ");
+                ime[i] = Console.ReadLine();
+                t[i] = new int [gbr];
+                Console.Write("to4ki  ");
+                for (int j = 0; j < gbr; j++)
+                {
+                   t[i][j] = int.Parse(Console.ReadLine());
+                }
+            }
+            Console.WriteLine("rezultat  ");
+            int naj_t = -1;
+            string dobur_igr = " ";
+            for (int i = 0; i < ibr; i++)
+            {
+                int sum = 0;
+                for (int j = 0; j < gbr; j++)
+                {
+                    sum += t[i][j];
+                }
+                Console.WriteLine(ime[i]+ "ob6to  "+ sum);
+                if (sum > naj_t)
+                {
+                    naj_t = sum;
+                    dobur_igr = ime[i];
+                }
+            }
+            Console.WriteLine("igra4  " +dobur_igr+ naj_t);
+        }
+    }
+}
